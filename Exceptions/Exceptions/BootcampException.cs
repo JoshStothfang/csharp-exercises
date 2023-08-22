16238c1e58918f83d5465979ff3555b6816b7df7
Josh Stothfang <josh@joshstothfang.com>
2023-08-22 10:45:55 -0400

    initial commit

---
+++ Exceptions/Exceptions/BootcampException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Exceptions
+{
+	public class BootcampException : Exception
+	{
+		public int Numerator { get; set; }
+		public int Denominator { get; set; }
+
+		//default constructor
+		public BootcampException() : base()
+		{
+		}
+
+		//constructor taking string (for message to display)
+		public BootcampException(string message) : base(message)
+		{
+		}
+
+		//constructor taking string & exception
+		public BootcampException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
+
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,56 @@
+﻿using Exceptions;
+
+method1();
+
+void method1()
+{
+    method2();
+}
+void method2()
+{
+    method3();
+}
+void method3()
+{
+    method4();
+}
+void method4()
+{
+    var d = 0;
+    var e = 1 / d;
+}
+
+
+/* //try,catch,finally
+int n = 1;
+int d = 0;
+
+try
+{
+    throw new Exception("WHY?");
+    var a = n / d;
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Don't divide by zero!");
+}
+catch (Exception)
+{
+    Console.WriteLine("This is a general exception!");
+}
+finally
+{
+    Console.WriteLine("This will always be executed!");
+}
+
+Console.WriteLine("Done...");
+*/
+/*
+if (d ==0)
+{
+    var bcex = new BootcampException($"Dividing by zero is a NO NO!");
+    bcex.Numerator = n;
+    bcex.Denominator = d;
+    throw bcex;
+}
+*/
