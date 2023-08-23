53da0e0823ac88ee0daac046c809b6c4a0264949
Josh Stothfang <josh@joshstothfang.com>
2023-08-22 22:38:44 -0400

    initial commit

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> names = new List<string>();
+//or var names = new List<string>();
+
+names.Add("Tony");
+names.Add("Megan");
+names.Add("Grant");
+names.Add("Drew");
+names.Add("David");
+names.Add("Josh");
+
+foreach(var name in names)
+{
+    Console.Write($"{name} ");
+}
+
+Console.WriteLine();
+names.Remove("Josh");
+
+foreach (var name in names)
+{
+    Console.Write($"{name} ");
+}
