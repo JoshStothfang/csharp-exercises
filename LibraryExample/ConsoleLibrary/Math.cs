3747a596405b1948d18a88bc0fd0fe0c041d19c8
Josh Stothfang <josh@joshstothfang.com>
2023-08-23 14:16:17 -0400

    initial commit

---
+++ LibraryExample/ConsoleLibrary/Math.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleLibrary;
+
+public class Math
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Sub(int x, int y)
+    {
+        return x - y;
+    }
+
+    public static int Mult(int x, int y)
+    {
+        return x * y;
+    }
+
+    public static int Div(int x, int y)
+    {
+        return x / y;
+    }
+
+    public static double Inverse(double a)
+    {
+        return 1 / a;
+    }
+
+    public static int Modulo(int a, int b)
+    {
+        var isZero = (b * (a / b));
+        if (isZero == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return a - (b * (a / b));
+        }
+    }
+
+    public static long Cubed(int a)
+    {
+        return a * a * a;
+    }
+
+    public static long Power(int x, int y)
+    {
+        var pow = 1;
+
+        for (int i = 1; i <= y; i++)
+        {
+            pow *= x;
+        }
+
+        return pow;
+        
+    }
+}
+
---
+++ LibraryExample/LibraryExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using clib = ConsoleLibrary;
+
+/*
+var x = 12;
+var y = 3;
+
+Console.WriteLine($"{x} + {y} = {clib.Math.Add(x,y)}");
+Console.WriteLine($"{x} - {y} = {clib.Math.Sub(x, y)}");
+Console.WriteLine($"{x} * {y} = {clib.Math.Mult(x, y)}");
+Console.WriteLine($"{x} / {y} = {clib.Math.Div(x, y)}");
+*/
+
+var x = 2;
+var y = 3;
+
+Console.WriteLine($"1 / {x} = {clib.Math.Inverse(x)}");
+Console.WriteLine($"{x} % {y} = {clib.Math.Modulo(x, y)}");
+Console.WriteLine($"{x} ^ 3 = {clib.Math.Cubed(x)}");
+
+Console.WriteLine($"{x} ^ {y} = {clib.Math.Power(x, y)}");
